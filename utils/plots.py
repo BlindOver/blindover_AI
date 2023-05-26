896d35cf9a3985ab60551a3e1aef93af742b4cdf
SangHo Kim <hop7311@naver.com>
2023-05-27 00:05:40 +0900

    Update plots.py

--- utils/plots.py
+++ utils/plots.py
@@ -1,3 +1,5 @@
+import torch
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -18,3 +20,24 @@ def plot_loss_graphs(history, project_name):
     plt.plot(np.arange(len(valid_acc)), valid_acc, label='valid')
     plt.legend(loc='best')
     plt.savefig(f'./runs/{project_name}/accuracy_graph.png')
+    
+    
+def plot_results(images, labels, outputs, project_name):
+    images = torch.cat(images, dim=0)
+    labels = torch.cat(labels, dim=0)
+    outputs = torch.cat(outputs, dim=0)
+
+    nod = len(outputs)
+    length = nod // 25 + 1
+
+    for i in range(length):
+        plt.figure(figsize=(20, 20))
+        for j in range(25):
+            idx = i * 25 + j
+            if idx > nod - 1:
+                break
+            plt.subplot(5, 5, j+1)
+            plt.imshow(images[idx].permute(1,2,0))
+            plt.axis('off')
+            plt.title(f'label: {labels[idx]}, output: {outputs[idx]}')
+        plt.savefig(f'./runs/test/{project_name}/result{i}.png')
