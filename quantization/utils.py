f02ea77191994af9490e392e7e63e1a11b0f2d2a
Sangh0 <hop7311@naver.com>
2023-07-05 16:45:45 +0900

    added feat: measure latency time

--- quantization/utils.py
+++ quantization/utils.py
@@ -1,8 +1,22 @@
 import os
+import time
 
 import torch
 import torchvision.models as models
 import torchvision.models.quantization as quantize_models
+
+
+def print_latency(process, req_return=False):
+    start = time.time()
+    if req_return:
+        result = process
+        end = time.time()
+        print(end - start: 3.f)
+        return result
+    else:
+        process
+        end = time.time()
+        print(end - start: 3.f)
 
 
 def print_size_of_model(model, label=''):
@@ -39,4 +53,4 @@ def comparison_size_of_models(model_name):
     float_model, quantized_model = load_models(model_name)
     f = print_size_of_model(float_model, 'float32')
     q = print_size_of_model(quantized_model, 'int8')
-    print("{0:.2f} times smaller".format(f/q))
+    print("{0:.2f} times smaller".format(f / q))
