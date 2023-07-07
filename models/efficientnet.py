582c68d4a88b5420ff1c4869d2dbeebf91ee0ad3
Sangh0 <hop7311@naver.com>
2023-07-07 21:34:51 +0900

    update quantization parameter

--- models/efficientnet.py
+++ models/efficientnet.py
@@ -1,15 +1,16 @@
 import torch 
 import torch.nn as nn
 import torchvision.models as models
+from torch.quantization import QuantStub, DeQuantStub
 
 # the number of trainable parameters: 20.3 M
 
 class EfficientNetV2(nn.Module):
     def __init__(
         self,
-        in_dim=3,
         num_classes=100,
         pre_trained=False,
+        quantization=False,
     ):
         super(EfficientNetV2, self).__init__()
         model = models.efficientnet_v2_s(pretrained=pre_trained)
@@ -22,9 +23,18 @@ class EfficientNetV2(nn.Module):
             nn.Linear(hidden_dim, num_classes),
         )
 
+        self.q = quantization
+        if quantization:
+            self.quant = QuantStub()
+            self.dequant = DeQuantStub()
+
     def forward(self, x):
+        if self.q:
+            x = self.quant(x)
         x = self.features(x)
         x = self.avgpool(x)
         x = x.view(x.size()[0], -1)
         x = self.classifier(x)
+        if self.q:
+            x = self.dequant(x)
         return x
