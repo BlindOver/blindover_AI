f4906d549f16756da901bf93db1d24bd801b638d
Sangh0 <hop7311@naver.com>
2023-07-08 01:14:28 +0900

    refactoring codes

--- quantization/quantization.py
+++ quantization/quantization.py
@@ -1,34 +1,34 @@
-import argparse
-
 import torch
 
 
-def load_model(model_name, weight, num_classes):
+def load_model(model_name, num_classes, quantization=True):
     if model_name == 'shufflenet':
         from ..models.shufflenet import ShuffleNetV2
-        model = ShuffleNetV2(num_classes=num_classes, pre_trained=False)
+        model = ShuffleNetV2(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     elif model_name == 'mobilenet':
         from ..models.mobilenet import MobileNetV3
-        model = MobileNetV3(num_classes=num_classes, pre_trained=False)
+        model = MobileNetV3(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     elif model_name == 'resnet18':
         from ..models.resnet import ResNet18
-        model = ResNet18(num_classes=num_classes, pre_trained=False)
+        model = ResNet18(num_classes=num_classes, pre_trained=False, quantization=quantization)
+
+    elif model_name == 'resnet50':
+        from ..models.resnet import ResNet50
+        model = ResNet50(num_classes=num_classes, pre_trained=False, quantization=quantization)
     
     elif model_name == 'efficientnet':
         from ..models.efficientnet import EfficientNetV2
-        model = EfficientNetV2(num_classes=num_classes, pre_trained=False)
+        model = EfficientNetV2(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     else:
         raise ValueError(f'{model_name} does not exists')
-
-    model = model.cpu()
-    model.load_state_dict(torch.load(weight, map_location=torch.device('cpu')))
     
     return model
 
 
+# for training
 def prepare_quantization(model):
     model.eval()
     model = model.cpu()
@@ -36,37 +36,20 @@ def prepare_quantization(model):
     return torch.quantization.prepare(model)
 
 
+# for training
 def model_quantization(model):
     model.eval()
     model = model.cpu()
     return torch.quantization.convert(model)
 
 
-def save_quantized_weight(quantized_model, weight_dir):
-    file_name = weight_dir.split('/')[-1]
-    save_dir = weight_dir.replace(file_name, f'quantized_{file_name}')
-    torch.save(quantized_model.state_dict(), save_dir)
-
-
-def get_args_parser():
-    parser = argparse.ArgumentParser(description='Model Quantization', add_help=False)
-    parser.add_argument('--model_name', type=str, required=True,
-                        help='model name')
-    parser.add_argument('--weight', type=str, required=True,
-                        help='a path of trained weight file')
-    parser.add_argument('--num_classes', type=int, default=33,
-                        help='the number of classes')
-    return parser
-
-
-def main(args):
-    model = load_model(args.model_name, args.weight, args.num_classes)
-    quantized_model = model_quantization(model)
-    save_quantized_weight(quantized_model, args.weight)
-    print('We have successfully completed the model quantization!')
-
-
-if __name__ == '__main__':
-    parser = argparse.ArgumentParser('Quantization', parents=[get_args_parser()])
-    args = parser.parse_args()
-    main(args)
+# for serving
+def quantization_serving(model_name: str, weight: str, num_classes: int=33):
+    model = load_model(model_name, num_classes, quantization=True)
+    model.eval()
+    model = model.cpu()
+    model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
+    model = torch.quantization.prepare(model)
+    model = torch.quantization.convert(model)
+    model.load_state_dict(torch.load(weight))
+    return model
