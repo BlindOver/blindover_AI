9f272ffe67bc3d745dc37e7230d4c72f0c900a5b
Sangh0 <hop7311@naver.com>
2023-07-07 21:35:04 +0900

    update quantization parameter

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -1,15 +1,16 @@
 import torch 
 import torch.nn as nn
 import torchvision.models as models
+from torch.quantization import QuantStub, DeQuantStub
 
 # the number of trainable parameters: 1.62 M
 
 class MobileNetV3(nn.Module):
     def __init__(
         self,
-        in_dim=3,
-        num_classes=100,
+        num_classes=33,
         pre_trained=True,
+        quantization=False,
     ):
         super(MobileNetV3, self).__init__()
         model = models.mobilenet_v3_small(pretrained=pre_trained)
@@ -25,9 +26,18 @@ class MobileNetV3(nn.Module):
             nn.Linear(hidden_dim, num_classes),
         )
 
+        self.q = quantization
+        if quantization:
+            self.quant = QuantStub()
+            self.dequant = DeQuantStub()
+
     def forward(self, x):
+        if self.q:
+            x = self.quant(x)
         x = self.features(x)
         x = self.avgpool(x)
         x = x.view(x.size()[0], -1)
         x = self.classifier(x)
+        if self.q:
+            x = self.dequant(x)
         return x
