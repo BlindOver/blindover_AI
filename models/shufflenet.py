f55cf3918974ca72966ac3c6c5a5d81fe867214e
Sangh0 <hop7311@naver.com>
2023-07-07 21:35:14 +0900

    update quantization parameter

--- models/shufflenet.py
+++ models/shufflenet.py
@@ -1,20 +1,31 @@
 import torch 
 import torch.nn as nn
 import torchvision.models as models
+from torch.quantization import QuantStub, DeQuantStub
 
 # the number of trainable parameters: 0.44 M
 
 class ShuffleNetV2(nn.Module):
     def __init__(
         self,
-        in_dim=3,
-        num_classes=100,
+        num_classes=33,
         pre_trained=True,
+        quantization=False,
     ):
         super(ShuffleNetV2, self).__init__()
         self.model = models.shufflenet_v2_x0_5(pretrained=pre_trained)
         hidden_dim = self.model.fc.in_features
         self.model.fc = nn.Linear(hidden_dim, num_classes)
 
+        self.q = quantization
+        if quantization:
+            self.quant = QuantStub()
+            self.dequant = DeQuantStub()
+
     def forward(self, x):
-        return self.model(x)
+        if self.q:
+            x = self.quant(x)
+        x = self.model(x)
+        if self.q:
+            x = self.dequant(x)
+        return x
