44f96ef2b1ea90983c178c2a3bc073e067b581a0
Sangh0 <hop7311@naver.com>
2023-07-24 22:34:31 +0900

    update quantization

--- quantization/quantization.py
+++ quantization/quantization.py
@@ -3,23 +3,23 @@ import torch
 
 def load_model(model_name, num_classes, quantization=True):
     if model_name == 'shufflenet':
-        from ..models.shufflenet import ShuffleNetV2
+        from models.shufflenet import ShuffleNetV2
         model = ShuffleNetV2(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     elif model_name == 'mobilenet':
-        from ..models.mobilenet import MobileNetV3
+        from models.mobilenet import MobileNetV3
         model = MobileNetV3(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     elif model_name == 'resnet18':
-        from ..models.resnet import ResNet18
+        from models.resnet import ResNet18
         model = ResNet18(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     elif model_name == 'resnet50':
-        from ..models.resnet import ResNet50
+        from models.resnet import ResNet50
         model = ResNet50(num_classes=num_classes, pre_trained=False, quantization=quantization)
     
     elif model_name == 'efficientnet':
-        from ..models.efficientnet import EfficientNetV2
+        from models.efficientnet import EfficientNetV2
         model = EfficientNetV2(num_classes=num_classes, pre_trained=False, quantization=quantization)
 
     else:
