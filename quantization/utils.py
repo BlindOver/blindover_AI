2407ae0ea2f56bcd725db1f08db08b8a0cc01165
Sangh0 <hop7311@naver.com>
2023-07-24 21:52:06 +0900

    modified errors

--- quantization/utils.py
+++ quantization/utils.py
@@ -3,7 +3,7 @@ import time
 
 import torch
 
-from quantization import load_model
+from .quantization import load_model
 
 
 def print_latency(process, req_return=False):
@@ -31,9 +31,9 @@ def comparison_size_of_models(model_name: str, num_classes: int=33):
     float_model = load_model(model_name, num_classes=num_classes, quantization=False)
     quantized_model = load_model(model_name, num_classes=num_classes, quantization=True)
     quantized_model.eval()
-    quantized_model = model.cpu()
+    quantized_model = quantized_model.cpu()
     quantized_model.qconfig = torch.quantization.get_default_qconfig('fbgemm')
-    quantized_model = torch.quantization.prepare(modquantized_modelel)
+    quantized_model = torch.quantization.prepare(quantized_model)
     quantized_model = torch.quantization.convert(quantized_model)
     f = print_size_of_model(float_model, 'float32')
     q = print_size_of_model(quantized_model, 'int8')
