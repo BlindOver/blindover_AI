56e992bc9e033596a330b97b44f88d8e89959405
Sangh0 <hop7311@naver.com>
2023-07-27 22:26:16 +0900

    modified errors of quantization process

--- quantization/quantization.py
+++ quantization/quantization.py
@@ -1,39 +1,19 @@
 import torch
 
 
-def load_model(model_name, num_classes, quantization=True):
-    if model_name == 'shufflenet':
-        from models.shufflenet import ShuffleNetV2
-        model = ShuffleNetV2(num_classes=num_classes, pre_trained=False, quantization=quantization)
-
-    elif model_name == 'mobilenet':
-        from models.mobilenet import MobileNetV3
-        model = MobileNetV3(num_classes=num_classes, pre_trained=False, quantization=quantization)
-
-    elif model_name == 'resnet18':
-        from models.resnet import ResNet18
-        model = ResNet18(num_classes=num_classes, pre_trained=False, quantization=quantization)
-
-    elif model_name == 'resnet50':
-        from models.resnet import ResNet50
-        model = ResNet50(num_classes=num_classes, pre_trained=False, quantization=quantization)
-    
-    elif model_name == 'efficientnet':
-        from models.efficientnet import EfficientNetV2
-        model = EfficientNetV2(num_classes=num_classes, pre_trained=False, quantization=quantization)
-
-    else:
-        raise ValueError(f'{model_name} does not exists')
-    
-    return model
-
-
 # for training
-def prepare_quantization(model, backend: str='x86'):
-    model.eval()
+def prepare_qat(model, backend: str='x86'):
+    model.train()
     model = model.cpu()
     model.qconfig = torch.quantization.get_default_qat_qconfig(backend)
-    return torch.quantization.prepare_qat(model, inplace=False)
+    return torch.quantization.prepare_qat(model)
+
+
+def prepare_ptq(model, backend: str = 'x86'):
+    model.eval()
+    model = model.cpu()
+    model.qconfig = torch.quantization.get_default_qconfig(backend)
+    return torch.quantization.prepare(model)
 
 
 # after training
