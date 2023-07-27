84b4747a974eb672e7eccf1b61ee3859451dc16c
Sangh0 <hop7311@naver.com>
2023-07-27 16:46:10 +0900

    modified the approach of loading pre-trained weight

--- models/shufflenet.py
+++ models/shufflenet.py
@@ -1,6 +1,7 @@
 import torch 
 import torch.nn as nn
 import torchvision.models as models
+from torchvision.models import ShuffleNet_V2_X0_5_Weights
 from torch.quantization import QuantStub, DeQuantStub
 
 # the number of trainable parameters: 0.44 M
@@ -10,22 +11,24 @@ class ShuffleNetV2(nn.Module):
         self,
         num_classes=33,
         pre_trained=True,
-        quantization=False,
+        quantize=False,
     ):
         super(ShuffleNetV2, self).__init__()
-        self.model = models.shufflenet_v2_x0_5(pretrained=pre_trained)
+        self.model = models.shufflenet_v2_x0_5(
+            weights=ShuffleNet_V2_X0_5_Weights.IMAGENET1K_V1 if pre_trained else None
+        )
         hidden_dim = self.model.fc.in_features
         self.model.fc = nn.Linear(hidden_dim, num_classes)
 
-        self.q = quantization
-        if quantization:
+        self.quantize = quantize
+        if quantize:
             self.quant = QuantStub()
             self.dequant = DeQuantStub()
 
     def forward(self, x):
-        if self.q:
+        if self.quantize:
             x = self.quant(x)
         x = self.model(x)
-        if self.q:
+        if self.quantize:
             x = self.dequant(x)
         return x
