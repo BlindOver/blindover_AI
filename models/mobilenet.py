329f1a1597250900c9dddb2863a84e71b66ffbd1
Sangh0 <hop7311@naver.com>
2023-07-27 16:46:05 +0900

    modified the approach of loading pre-trained weight

--- models/mobilenet.py
+++ models/mobilenet.py
@@ -1,7 +1,6 @@
 import torch 
 import torch.nn as nn
-import torchvision.models as models
-from torch.quantization import QuantStub, DeQuantStub
+from torchvision.models import mobilenet_v3_small, MobileNet_V3_Small_Weights
 
 # the number of trainable parameters: 1.62 M
 
@@ -10,10 +9,11 @@ class MobileNetV3(nn.Module):
         self,
         num_classes=33,
         pre_trained=True,
-        quantization=False,
     ):
         super(MobileNetV3, self).__init__()
-        model = models.mobilenet_v3_small(pretrained=pre_trained)
+        model = mobilenet_v3_small(
+            weights=MobileNet_V3_Small_Weights.IMAGENET1K_V1 if pre_trained else None
+        )
         self.features = model.features
         self.avgpool = model.avgpool
         
@@ -26,18 +26,9 @@ class MobileNetV3(nn.Module):
             nn.Linear(hidden_dim, num_classes),
         )
 
-        self.q = quantization
-        if quantization:
-            self.quant = QuantStub()
-            self.dequant = DeQuantStub()
-
     def forward(self, x):
-        if self.q:
-            x = self.quant(x)
         x = self.features(x)
         x = self.avgpool(x)
-        x = x.view(x.size()[0], -1)
+        x = torch.flatten(x, dim=1)
         x = self.classifier(x)
-        if self.q:
-            x = self.dequant(x)
         return x
